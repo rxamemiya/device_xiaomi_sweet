9dff961b2154f498de827a980ef4300cc9e2d93e
rxamemiya <ngopisantuy76@gmail.com>
2026-02-23 01:55:49 -0800

    sweet: update repo
    Signed-off-by: rxamemiya nazunaamemiya@noname.dev

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,16 @@
-# sm6150 common
-git clone https://github.com/LineageOS/android_device_xiaomi_sm6150-common.git
+# Common Sm6150
+git clone https://github.com/LineageOS/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+
+# Kernel
+git clone https://github.com/PixelOS-Devices/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+
+# Sweet
+git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+
+# Hardware Xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+
+# Miui Camera
+git clone https://github.com/PixelOS-Devices/android_device_xiaomi_miuicamera-sweet.git device/xiaomi/miuicamera-sweet
+git clone git@gitlab.com:NoPrincessHere/proprietary_vendor_xiaomi_miuicamera-sweet.git vendor/xiaomi/miuicamera-sweet
