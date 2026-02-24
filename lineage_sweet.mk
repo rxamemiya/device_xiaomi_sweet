1a8a148c9b2976bd3083b8dabad9b47091685327
rxamemiya <ngopisantuy76@gmail.com>
2026-02-24 03:32:36 -0800

    sweet: add flags TARGET_DISABLE_EPPE

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -13,6 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_sweet
